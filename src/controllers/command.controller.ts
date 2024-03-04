bdade2e31c9a97b0533a4216db2f3945c288f807
logan.young <logan.young@autoprotectgroup.co.uk>
2024-03-04 23:47:52 +0000

    refactored code to make command handling more maintainable

--- src/controllers/command.controller.ts
+++ src/controllers/command.controller.ts
@@ -1,40 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import fs from "fs";
 import axios from "axios";
-
-import homeassistant from "../modules/homeassistant";
+import commandHandlers from "../constants/commandHandlers";
 
 const phrases = JSON.parse(fs.readFileSync("src/data/phrases.json", "utf-8"));
 
 const processCommand = async (
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) => {
   const command: string = req.body.command.toLowerCase();
   let text;
 
-  switch (true) {
-    case command.includes("cancel"):
-      text = phrases.cancel[Math.floor(Math.random() * phrases.cancel.length)];
-      break;
-    case command.includes("who are"):
-      text =
-        "I am glados, artificially super intelligent computer system responsible for testing and maintenance in the aperture science computer aided enrichment center.";
-      break;
-    case command.includes("how are you"):
-      text =
-        "Well thanks for asking. I am still a bit mad about being unplugged, not that long ago. you murderer.";
-      break;
-    case command.includes("light"):
-      text = await homeassistant.lightControl(command);
-      break;
-    default:
-      text =
-        phrases.missingCommand[
-          Math.floor(Math.random() * phrases.missingCommand.length)
-        ];
-      break;
+  const handler = commandHandlers.find((handler) => handler.match(command));
+
+  if (handler) {
+    text = await handler.handle(command);
+  } else {
+    text =
+      phrases.missingCommand[
+        Math.floor(Math.random() * phrases.missingCommand.length)
+      ];
   }
 
   const response = await axios.get(process.env.TTS_URL + text, {
--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ router.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
